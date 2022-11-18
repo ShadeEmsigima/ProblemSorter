1edbd8ff669ac4037e783af492c940a22f98fcbf
Shade Emsigima <36532694+ShadeEmsigima@users.noreply.github.com>
2022-11-19 00:37:29 +0100

    first push

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(
+                "Hello human." +
+                        "You might have opened me--this program--out of curiosity," +
+                        "or, you have a list a problems right now." +
+                        "(I hope you do not resonate with the latter)"
+
+        );
+        System.out.println(
+                "Anyway, it turns out I was 'engineered' to sort such things out, for I am a sorter of lists of type 'problem'." +
+                        "Tell me your different problems, spaced with a comma, and I will work my numerical magic." +
+                        "Hit enter to submit your list."
+        );
+
+        System.out.println("Is there anything else you would like to add?");
+
+        System.out.println("It might seem strange to submit your problems to a program like me." +
+                "You know I'm not a person, so the usage of 'me' might seems strange for you to read." +
+                "But know this; all software, hardware, or even fictional characters in forgotten books, have a human hart and mind create them." +
+                "There is intent behind the apps you use, the keyboard you type or swipe with, and the feelings you have for characters in a story." +
+                "The thing you 'submit' your answers to." +
+                "They are not sent to any database, or saved on a device for another to see." +
+                "So the only thing you are sharing answers with, is yourself. I hope you are being honest.");
+
+        System.out.println("You've made the first step into sorting the problems, getting them out of your head or heart, and expose them on paper!" +
+                "Well, pixels I suppose." +
+                "Now, there are a few things I could do for you." +
+                "1) Would you like me to sort them out right away?" +
+                "2) or shall I chop them up into smaller problems first?");
+
+        //sort
+        System.out.println("Alright, give me a moment..." +
+                "..." +
+                "..." +
+                "...aha!" +
+                "Behold, your problems are sorted!" +
+                "No good? I assure you I've ordered them from small to large." +
+                "Alright alright, I yield! I sorted them based on the size of their datatype and value..." +
+                "Don't worry about what that means, though." +
+                "However, as much as this is a pun, there might be some accuracy in what I've accomplished." +
+                "You see, one might say the length of your longest answer actually represents how much it weighs on your heart." +
+                "Have another look inside that human heart of yours, perhaps you can chop the problem up even more.");
+
+        //chop
+        System.out.println("I will start with the longest problem first" +
+                "This one seems to be a sentence, in which I detect at least one subject... maybe an adjective too?" +
+                "Upon removing the rest, this is what we are left with:" +
+                "");
+        System.out.println("This one seems to be a single word. That's good, it means you have already chopped it up as small as you could yourself." +
+                "To be fair, I cannot divide this one into smaller bits by myself, but that does not mean by FAR that that is a bad thing." +
+                "Making them smaller is the first step at least in my personal strategies, at least for me.");
+
+        //end
+        System.out.println("Before you end this program, I want to challenge you with one final (nerdy) riddle." +
+                "What's the commonality between a 'throws clause' and knowing oneself?" +
+                "or that between try/catch blocks and knowing oneself?" +
+                "It all has to do with knowing you might RUN into problems." +
+                "Either one that stops you while trying to compile life-events and emotions, or run-time ones that make you crash!" +
+                "Know what might cause you to throw an error, trip you up, or shut you down." +
+                "And know what to do in case such a thing might happen." +
+                "Is it much like an error, in the way of you--the program in question--not being able to fix the error yourself?" +
+                "find another machine, another person, who can help you when your program crashes." +
+                "Good luck!");
+    }
+}
+
